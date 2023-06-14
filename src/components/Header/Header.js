829a7e77a212c58cbfcab39ef4248e49941f8d20
h1ze <burgov@mail.ru>
2023-06-14 19:05:50 +0400

    feat: создать наброски Header для /movies

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <Route
         path="/"
         element={
-          <div className="header">
+          <div className="header header_theme_dark">
             <div className="header__wrapper">
               <nav className="header__menu">
                 <Link to="/">
@@ -29,6 +29,47 @@ function Header() {
                     </Link>
                   </li>
                 </ul>
+              </nav>
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="movies"
+        element={
+          <div className="header">
+            <div className="header__wrapper">
+              <nav className="header__menu">
+                <Link to="/">
+                  <img src={logo} className="header__logo" alt="Логотип" />
+                </Link>
+                <div className="header__links-container">
+                  <ul className="header__movies-links">
+                    <li>
+                      <Link
+                        to="movies"
+                        className="header__movies-link"
+                        target="_blank"
+                      >
+                        Фильмы
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="saved-movies"
+                        className="header__movies-link"
+                        target="_blank"
+                      >
+                        Сохраненные фильмы
+                      </Link>
+                    </li>
+                  </ul>
+                  <Link to="profile" className="header__profile-link">
+                    Аккаунт
+                    <div className="header__profile-icon"></div>
+                  </Link>
+                </div>
+                <button className="header__burger-btn"></button>
               </nav>
             </div>
           </div>
