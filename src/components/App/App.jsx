2890af3b00a7740168cddafa32b7fd92ee3f65da
h1ze <burgov@mail.ru>
2023-06-15 12:04:31 +0400

    feat: Создать компонент Promo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Promo />
       {/* <Main />
         <Footer /> */}
     </div>
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Promo.css';
+
+const Promo = () => {
+  return (
+    <div className="promo">
+      <div className="promo__wrapper">
+        <div className="promo__background">
+          <h1 className="promo__title">
+            Учебный проект студента факультета Веб-разработки.
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Promo;
