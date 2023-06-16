084c5134ef49f5e87f2443f60de6de8f3632dd00
h1ze <burgov@mail.ru>
2023-06-16 11:52:46 +0400

    fix: добавить wrapper в Techs и AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -5,37 +5,39 @@ import('./AboutProject.css');
 const AboutProject = () => {
   return (
     <section className="about-project">
-      <SectionHeading>О проекте</SectionHeading>
-      <div className="about-project__content">
-        <div className="about-project__chapter">
-          <h3 className="about-project__chapter-title">
-            Дипломный проект включал 5 этапов
-          </h3>
-          <p className="about-project__chapter-text">
-            Составление плана, работу над бэкендом, вёрстку, добавление
-            функциональности и финальные доработки.
-          </p>
-        </div>
-        <div className="about-project__chapter">
-          <h3 className="about-project__chapter-title">
-            На выполнение диплома ушло 5 недель
-          </h3>
-          <p className="about-project__chapter-text">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-            соблюдать, чтобы успешно защититься.
-          </p>
-        </div>
-      </div>
-      <div className="about-project__scale-container">
-        <div className="about-project__scale-column about-project__scale-column_size_s">
-          <div className="about-project__scale-part">1 неделя</div>
-          <span className="about-project__scale-caption">Backend</span>
-        </div>
-        <div className="about-project__scale-column about-project__scale-column_size_b">
-          <div className="about-project__scale-part about-project__scale-part_type_frontend">
-            4 недели
+      <div className="about-project__wrapper">
+        <SectionHeading>О проекте</SectionHeading>
+        <div className="about-project__content">
+          <div className="about-project__chapter">
+            <h3 className="about-project__chapter-title">
+              Дипломный проект включал 5 этапов
+            </h3>
+            <p className="about-project__chapter-text">
+              Составление плана, работу над бэкендом, вёрстку, добавление
+              функциональности и финальные доработки.
+            </p>
           </div>
-          <span className="about-project__scale-caption">Frontend</span>
+          <div className="about-project__chapter">
+            <h3 className="about-project__chapter-title">
+              На выполнение диплома ушло 5 недель
+            </h3>
+            <p className="about-project__chapter-text">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+              соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+        <div className="about-project__scale-container">
+          <div className="about-project__scale-column about-project__scale-column_size_s">
+            <div className="about-project__scale-part">1 неделя</div>
+            <span className="about-project__scale-caption">Backend</span>
+          </div>
+          <div className="about-project__scale-column about-project__scale-column_size_b">
+            <div className="about-project__scale-part about-project__scale-part_type_frontend">
+              4 недели
+            </div>
+            <span className="about-project__scale-caption">Frontend</span>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -6,21 +6,23 @@ const Techs = () => {
   const techs = ['HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoDB'];
   return (
     <section className="techs">
-      <SectionHeading>Технологии</SectionHeading>
-      <div className="techs__content">
-        <h3 className="techs__content-title">7 технологий</h3>
-        <p className="techs__content-subtitle">
-          На курсе веб-разработки мы освоили технологии, которые применили в
-          дипломном проекте.
-        </p>
-        <div className="techs__blocks">
-          {techs.map((tech, index) => {
-            return (
-              <div className="techs__block" key={index}>
-                {tech}
-              </div>
-            );
-          })}
+      <div className="techs__wrapper">
+        <SectionHeading>Технологии</SectionHeading>
+        <div className="techs__content">
+          <h3 className="techs__content-title">7 технологий</h3>
+          <p className="techs__content-subtitle">
+            На курсе веб-разработки мы освоили технологии, которые применили в
+            дипломном проекте.
+          </p>
+          <div className="techs__blocks">
+            {techs.map((tech, index) => {
+              return (
+                <div className="techs__block" key={index}>
+                  {tech}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
