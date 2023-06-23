059c2ba9a90e80518665de672405fe1c35e8a3fd
h1ze <burgov@mail.ru>
2023-06-23 10:44:30 +0400

    feat: добавить адреса ссылок в Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,19 +9,31 @@ const Portfolio = () => {
       <nav>
         <ul className="portfolio__list">
           <li className="portfolio__item">
-            <Link className="portfolio__link" to="#">
+            <Link
+              className="portfolio__link"
+              to="https://h1ze.github.io/how-to-learn/"
+              target="_blank"
+            >
               Статичный сайт
               <div className="portfolio__link-icon"></div>
             </Link>
           </li>
           <li className="portfolio__item">
-            <Link className="portfolio__link" to="#">
+            <Link
+              className="portfolio__link"
+              to="https://h1ze.github.io/russian-travel/"
+              target="_blank"
+            >
               Адаптивный сайт
               <div className="portfolio__link-icon"></div>
             </Link>
           </li>
           <li className="portfolio__item">
-            <Link className="portfolio__link" to="#">
+            <Link
+              className="portfolio__link"
+              to="https://h1ze.github.io/mesto/"
+              target="_blank"
+            >
               Одностраничное приложение
               <div className="portfolio__link-icon"></div>
             </Link>
