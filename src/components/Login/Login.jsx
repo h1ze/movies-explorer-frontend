ba572ecda7cb226ec5708fcdea571da08754c69e
h1ze <burgov@mail.ru>
2023-06-24 13:11:44 +0400

    fix: убрать неиспользуемые импорты

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AuthForm from '../AuthForm/AuthForm';
-import './Login.css';
 
 const Login = () => {
   const loginFormData = {
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AuthForm from '../AuthForm/AuthForm';
-import './Register.css';
 
 const Register = () => {
   const registerFormData = {
